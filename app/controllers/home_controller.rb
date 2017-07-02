862995f8565b2804c04034ffff3338446f0f8652
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2017-07-02 16:06:27 -0400

    Redirect to activity entry screen if authenticated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  before_action :redirect_if_authenticated
+
   def index
+  end
+
+  private
+  def redirect_if_authenticated
+    if current_user.present?
+      redirect_to new_activity_entry_path
+    end
   end
 end
